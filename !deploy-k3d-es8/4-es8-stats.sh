d33e7cb27c2bc5464f4742148f4bc2f2bd07ce79
Aryan Blaauw <ablaauw@gmx.net>
2022-06-12 17:36:36 +0000

    Update 4-es8-stats.sh

--- !deploy-k3d-es8/4-es8-stats.sh
+++ !deploy-k3d-es8/4-es8-stats.sh
@@ -2,7 +2,7 @@
 echo $(kubectl get secret es8-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 PASSWORD=$(kubectl get secret es8-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 
-kubectl port-forward service/es8-es-http 9200 &
-kubectl port-forward service/es8-kb-http 5601 &
+kubectl port-forward service/es8-es-http 9200 --address='0.0.0.0' &
+kubectl port-forward service/es8-kb-http 5601 --address='0.0.0.0' &
 
 curl -u "elastic:$PASSWORD" -k "https://localhost:9200"
