0ca941344d2791c58ddef644baedaff4ff0a9b79
Aryan Blaauw <ablaauw@gmx.net>
2022-06-12 16:31:24 +0000

    Update 1-k3d-eck.sh

--- !deploy-k3d-es8/1-k3d-eck.sh
+++ !deploy-k3d-es8/1-k3d-eck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-k3d cluster create es80 --servers 1 --agents 2 --port '9200:9200@agent:1' --port '9300:9300@agent:1' --port '5601:5601@agent:1' --port '8081:80@loadbalancer'
+# k3d cluster create es80 --servers 1 --agents 2 --port '8081:80@loadbalancer'
 kubectl create -f https://download.elastic.co/downloads/eck/2.2.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.2.0/operator.yaml
 kubectl -n elastic-system logs -f statefulset.apps/elastic-operator
