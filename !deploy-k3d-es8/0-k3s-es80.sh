ec58ffb1ebc2407db2f34109cff252d0bdae0403
Aryan Blaauw <ablaauw@gmx.net>
2022-06-26 12:13:44 +0200

    Update 0-k3s-es80.sh

--- !deploy-k3d-es8/0-k3s-es80.sh
+++ !deploy-k3d-es8/0-k3s-es80.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+snap install kubectl --classic
 k3d cluster create es80 --servers 1 --agents 2 --port '8081:80@loadbalancer'
