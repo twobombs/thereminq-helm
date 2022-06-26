208e794aa394ce8430d1cdef6b4a7bcd352f81cf
Aryan Blaauw <ablaauw@gmx.net>
2022-06-26 12:29:15 +0200

    Update 1-k3d-eck.sh

--- !deploy-k3d-es8/1-k3d-eck.sh
+++ !deploy-k3d-es8/1-k3d-eck.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# k3d cluster create es80 --servers 1 --agents 2 --port '8081:80@loadbalancer'
+# deploy ECK and ES agent
 kubectl create -f https://download.elastic.co/downloads/eck/2.2.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.2.0/operator.yaml
 kubectl -n elastic-system logs -f statefulset.apps/elastic-operator
+#
+wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.2.2-amd64.deb
+dpkg -i elastic-agent-8.2.2-amd64.deb
